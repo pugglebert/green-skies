9653e3beff0d713728e3ed8a711be3001e48924a
Nathan Huynh <tnh33@uclive.ac.nz>
2020-07-31 10:12:38 +1200

    Fix test failure

--- src/test/java/seng202/team1/EnyangTest.java
+++ src/test/java/seng202/team1/EnyangTest.java
@@ -3,5 +3,7 @@ package seng202.team1;
 import junit.framework.TestCase;
 
 public class EnyangTest extends TestCase {
-
+    public void test(){
+        assertEquals(1,1);
+    }
 }
--- src/test/java/seng202/team1/NathanTest.java
+++ src/test/java/seng202/team1/NathanTest.java
@@ -3,5 +3,8 @@ package seng202.team1;
 import junit.framework.TestCase;
 
 public class NathanTest extends TestCase {
+    public void test(){
+        assertEquals(1,1);
+    }
 
 }
