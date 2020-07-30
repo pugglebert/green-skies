1b3895ac18156ded4ae6baed2547776525f2a2e7
He zhengjingrui <zhe25@uclive.ac.nz>
2020-07-31 10:16:15 +1200

    Enyang testcase

--- src/test/java/seng202/team1/EnyangTest.java
+++ src/test/java/seng202/team1/EnyangTest.java
@@ -1,7 +1,10 @@
 package seng202.team1;
 
 import junit.framework.TestCase;
+import junit.framework.TestResult;
 
 public class EnyangTest extends TestCase {
-
+    public void testTestcase() {
+        assertEquals(1,1);
+    }
 }
