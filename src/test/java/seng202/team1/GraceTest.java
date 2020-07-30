e7dcf85eac14ae75d24e2c21d4e1c5b5793825b2
Grace Hanlon <gmh91@uclive.ac.nz>
2020-07-31 10:14:31 +1200

    Grace Hanlon another JUnit commit

--- src/test/java/seng202/team1/GraceTest.java
+++ src/test/java/seng202/team1/GraceTest.java
@@ -1,7 +1,14 @@
 package seng202.team1;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class GraceTest extends TestCase {
+
+    @Test
+    void GraceTest() {
+        assertEquals(1, 1);
+    }
+
 
 }
