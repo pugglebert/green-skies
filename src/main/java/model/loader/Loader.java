48827bc711edbf4205ef25e95ac713a36feff897
Ella Johnson <egj21@uclive.ac.nz>
2020-08-09 11:25:34 +1200

    openFile and checkFile written for Loader class. checkFile tests written for LoaderTest.

--- src/main/java/model/loader/Loader.java
+++ src/main/java/model/loader/Loader.java
@@ -1,4 +1,63 @@
 package model.loader;
 
+import java.io.File;
+import java.nio.file.FileSystemException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static com.google.common.io.Files.getFileExtension;
+
+/**
+ * The Loader class checks that a file has the expected extension, opens that file and creates an instance of the
+ * Parser class to parse the file
+ *
+ * @author Ella Johnson
+ * @version 1.0
+ * @since 2020-08-09
+ */
 public class Loader {
+
+
+    /** Expected file extension*/
+    private String csvExtension = "csv";
+    private ArrayList<String> lines;
+    private Parser parser;
+
+    public Loader() {
+
+    }
+
+    /** Calls checkFileType. Opens file. Seperates lines in file into ArrayList of Strings. Instantiates Parser.
+     * @param fileName name of the file to be opened
+     */
+    public void openFile(String fileName) {
+
+        try {
+            checkFileType(fileName);
+        } catch (FileSystemException e) {
+            System.out.println(e.getMessage()); //Change later, just for debugging
+        }
+
+        lines = new ArrayList<String>();
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()); {
+            lines.add(scanner.nextLine());
+        }
+
+        parser = new Parser(lines);
+    }
+
+    /**
+     * Checks if file extension is csv.
+     * @param fileName Name of file to be checked.
+     * @throws FileSystemException
+     */
+    public void checkFileType(String fileName) throws FileSystemException {
+        String extension = getFileExtension(fileName);
+        if (extension != csvExtension) {
+            throw new FileSystemException("Unsupported file type. Only CSV files can be processed.");
+        }
+    }
 }
--- src/test/java/model/loader/LoaderTest.java
+++ src/test/java/model/loader/LoaderTest.java
@@ -1,7 +1,45 @@
 package model.loader;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.FileSystemException;
+
 import static org.junit.Assert.*;
 
 public class LoaderTest {
+
+    private Loader loader;
+
+    @Before
+    public void setUp() {
+        loader = new Loader();
+    }
+
+    @Test
+    /** Test that openFile instantiates the Parser class with an ArrayList of Lines matching the contents of the file */
+    public void testOpenFile() {
+
+    }
+
+    @Test
+    /** Test that checkFileType throws an exception when called with a filename with an invalid extension */
+    public void testCheckFileTypeInvalidFileName() {
+        try {
+            loader.checkFileType("badFile.jpg");
+            fail();
+        } catch (FileSystemException e) { }
+    }
+
+    @Test
+    /** Test that checkFileType doesn't throw an exception when called with a filename with a valid extension */
+    public void testCheckFileTypeValidFileName() {
+        try {
+            loader.checkFileType("goodFile.csv");
+        } catch (FileSystemException e) {
+            fail();
+        }
+    }
+
 
 }
