0648a1f3f60bece1a5ee9f67164eda91280f1100
Ella Johnson <egj21@uclive.ac.nz>
2020-08-09 12:42:23 +1200

    Created placeholder classes for flightParser and RouteParser.

---
+++ src/main/java/model/loader/RouteParser.java
@@ -0,0 +1,9 @@
+package model.loader;
+
+import java.util.ArrayList;
+
+public class RouteParser extends Parser {
+    public RouteParser(ArrayList<String> dataFile) {
+        super(dataFile);
+    }
+}
