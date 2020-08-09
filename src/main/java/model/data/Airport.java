5e27e15410b9b384382ac56b37b1c5ae0d1e7fc1
Enyang <ezh15@uclive.ac.nz>
2020-08-09 14:24:47 +1200

    Finish parser and now need to implement validater

--- src/main/java/model/data/Airport.java
+++ src/main/java/model/data/Airport.java
@@ -7,11 +7,25 @@ public class Airport {
     String country;
     String IATA;
     String ICAO;
-    Float latitude;
-    Float longitude;
+    float latitude;
+    float longitude;
     int altitude;
-    Float timezone;
+    float timezone;
     String DST;
     String dataBaseTimeZone;
 
+    public Airport(int airportID, String name, String city, String country, String IATA, String ICAO, float latitude, float longitude, int altitude, float timezone, String DST, String dataBaseTimeZone){
+        this.airportID = airportID;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.IATA = IATA;
+        this.ICAO = ICAO;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
+        this.timezone = timezone;
+        this.DST = DST;
+        this.dataBaseTimeZone = dataBaseTimeZone;
+    }
 }
--- src/main/java/model/loader/AirportParser.java
+++ src/main/java/model/loader/AirportParser.java
@@ -1,30 +1,42 @@
 package model.loader;
 
+import model.data.Airline;
 import model.data.Airport;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class AirportParser extends Parser {
-    private HashSet<Airport> AirportLine;
+    private HashSet<Airport> airports;
 
     public AirportParser(ArrayList<String> dataFile) {
         super(dataFile);
     }
 
     private void dataParse(){
-        for (String dataLine: dataFile){
-            String[] line= dataLine.split(",");
-
+//        for (String dataLine: dataFile){
+//            String[] line= dataLine.split(",");
+        String dataline = "4,Nadzab,Nadzab,Papua New Guinea,LAE,AYNZ,-6.569828,146.726242,239,10,U,Pacific/Port_Moresby";
+        String[] line = dataline.split(",");
+        if (validater(line)){
+            Airport airport = new Airport(Integer.parseInt(line[0]), line[1], line[2], line[3], line[4], line[5], Float.parseFloat(line[6]), Float.parseFloat(line[7]), Integer.parseInt(line[8]), Float.parseFloat(line[9]), line[10], line[11]);
+            airports.add(airport);
+        } else {
+            System.out.println("Invalid data format");
         }
+        }
+//    }
+
+    private Boolean validater(String[] line) {
+//        Boolean isValiate = true;
+//        try{
+//            line[0] = Integer.parseInt(line[0]);
+//        }
+        return true;
     }
 
-//    private Boolean validater(String[] line) {
-//
-//    }
-//
-//    public static void main(String[] args){
-//        String[] test =
-//
-//    }
+    public HashSet<Airport> getAirports() {
+        return airports;
+    }
+
 }
