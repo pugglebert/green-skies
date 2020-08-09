d01b8ebc2746dfd80846a7214c4754ddb2f9df6b
Enyang <ezh15@uclive.ac.nz>
2020-08-09 15:52:51 +1200

    Implemented AirportParser without testing

--- src/main/java/model/loader/AirportParser.java
+++ src/main/java/model/loader/AirportParser.java
@@ -13,28 +13,27 @@ public class AirportParser extends Parser {
     }
 
     private void dataParse(){
-//        for (String dataLine: dataFile){
-//            String[] line= dataLine.split(",");
-        String dataline = "4,Nadzab,Nadzab,Papua New Guinea,LAE,AYNZ,-6.569828,146.726242,239,10,U,Pacific/Port_Moresby";
-        String[] line = dataline.split(",");
-        if (validater(line)){
-            try{
-                Airport airport = new Airport(Integer.parseInt(line[0]), line[1], line[2], line[3], line[4], line[5], Float.parseFloat(line[6]), Float.parseFloat(line[7]), Integer.parseInt(line[8]), Float.parseFloat(line[9]), line[10], line[11]);
-                airports.add(airport);
-            } catch(Exception e) {
-                System.out.println("Unknown Error.");
-            }
+        for (String dataLine: dataFile){
+            String[] line= dataLine.split(",");
+//        String dataline = "4,Nadzab,Nadzab,Papua New Guinea,LAE,AYNZ,-6.569828,146.726242,239,10,U,Pacific/Port_Moresby";
+//        String[] line = dataline.split(",");
+            if (validater(line)){
+                try{
+                    Airport airport = new Airport(Integer.parseInt(line[0]), line[1], line[2], line[3], line[4], line[5], Float.parseFloat(line[6]), Float.parseFloat(line[7]), Integer.parseInt(line[8]), Float.parseFloat(line[9]), line[10], line[11]);
+                    airports.add(airport);
+                } catch(Exception e) {
+                    System.out.println("Unknown Error.");
+                }
 
-        } else {
-            System.out.println("Unable to insert data.");
+            } else {
+                System.out.println("Unable to insert data.");
+            }
         }
-        }
-//    }
+    }
 
     private Boolean validater(String[] line) {
-
+        // airport ID Duplication check
         for(Airport airport: airports){
-            // airport ID Duplication check
             try{
                 if(airport.airportID == Integer.parseInt(line[0])){
                     System.out.println("Duplicated airport ID.");
@@ -43,32 +42,84 @@ public class AirportParser extends Parser {
             } catch (Exception e){
                 System.out.println("Unknown type of airport ID.");
             }
+        }
 
-            // airport name check
-            if(!airport.name.matches("[a-zA-Z ]+")){
-                System.out.println("Unknown type of airport name.");
-                return false;
+        // airport name check
+        if(!line[1].matches("[a-zA-Z ]+")){
+            System.out.println("Unknown type of airport name.");
+            return false;
+        }
+
+        //airport city check
+        if(!line[2].matches("[a-zA-Z ]+")){
+            System.out.println("Unknown type of airport city.");
+            return false;
+        }
+
+        //airport country check
+        //ISO 3166-1 codes not implemented
+        if(!line[3].matches("[a-zA-Z ]+")){
+            System.out.println("Unknown type of airport country.");
+            return false;
+        }
+
+        //airport IATA check
+        if(!(line[4].toLowerCase().equals("null") || line[4].toLowerCase().equals("unknown"))){
+        if(!line[4].matches("[a-zA-Z]+" ) || line[4].length() != 3 ){
+            System.out.println("Unknown type of airport IATA.");
+            return false;
             }
+        }
 
-            //airport city check
-            if(!airport.city.matches("[a-zA-Z ]+")){
-                System.out.println("Unknown type of airport city.");
-                return false;
-            }
-
-            //airport country check
-            //ISO 3166-1 codes not implemented
-            if(!airport.country.matches("[a-zA-Z ]+")){
-                System.out.println("Unknown type of airport country.");
-                return false;
-            }
-
-            //airport IATA check
-            if(!airport.IATA.matches("[a-zA-Z]+" || )){
-                System.out.println("Unknown type of airport IATA.");
+        //airport ICAO check
+        if(!(line[5].toLowerCase().equals("null") || line[5].toLowerCase().equals("unknown"))){
+            if(!line[5].matches("[a-zA-Z]+" ) || line[5].length() != 4 ){
+                System.out.println("Unknown type of airport ICAO.");
                 return false;
             }
         }
+
+        //airport Latitude check
+        try{
+            Float.parseFloat(line[6]);
+        } catch (Exception e){
+            System.out.println("Unknown type of airport latitude.");
+        }
+
+        //airport longitude check
+        try{
+            Float.parseFloat(line[7]);
+        } catch (Exception e){
+            System.out.println("Unknown type of airport longitude.");
+        }
+
+        //airport altitude check
+        try{
+            Integer.parseInt(line[8]);
+        } catch (Exception e){
+            System.out.println("Unknown type of airport altitude.");
+        }
+
+        //airport time zone check
+        try{
+            Float.parseFloat(line[9]);
+        } catch (Exception e){
+            System.out.println("Unknown type of airport time zone.");
+        }
+
+        //airport DST check
+        if(!line[10].matches("[EASOZNU]+" ) || line[10].length() != 1 ){
+            System.out.println("Unknown type of airport DST.");
+            return false;
+        }
+
+        //airport database time zone check
+        if(!line[11].matches("[a-zA-Z/a-zA-Z_]+" )){
+            System.out.println("Unknown type of airport database time zone.");
+            return false;
+        }
+
+        //--------------------------------------
         System.out.println("Data validated.");
         return true;
     }
