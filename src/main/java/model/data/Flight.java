a652f09e8cdca2b43aad4cf16bddc96125ae2a6f
Hayley Krippner <hek21@uclive.ac.nz>
2020-08-10 22:21:06 +1200

    Commenting for Flight class.

--- src/main/java/model/data/Flight.java
+++ src/main/java/model/data/Flight.java
@@ -1,5 +1,11 @@
 package model.data;
 
+/**
+ * The Flight class for containing all data for one specific flight.
+ * @author Hayley Krippner
+ * @version 1.1
+ * @since 2020-08-09
+ */
 public class Flight {
     private String flightID;
     private String type;
@@ -9,6 +15,9 @@ public class Flight {
     private int distance;
     private String flightName;
 
+    /**
+     * Constructor for Flight. Passes current flight data to attributes.
+     */
     public Flight(String flightID, String type, String airportsVia, int altitude, int position, int distance,
                   String flightName) {
         this.flightID = flightID;
