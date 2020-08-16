ddd5d5a6315b02a62a9555cf61c6989517e5ce71
He zhengjingrui <zhe25@uclive.ac.nz>
2020-08-16 20:34:10 +1200

    Tom made button works

--- src/main/java/controller/main/WelcomeController.java
+++ src/main/java/controller/main/WelcomeController.java
@@ -1,6 +1,8 @@
 package controller.main;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,16 +17,18 @@ public class WelcomeController {
     }
 
     //Method to handle the event for continuing to the info screen of the application.
-    public void toInfoScreen() throws IOException {
-        Stage stage = new Stage(); // create a stage for scene
+    public void toInfoScreen(ActionEvent event) throws IOException {
+
 
         Parent root = FXMLLoader.load(getClass().getResource("help.fxml")); // load help.fxml
 
         Scene scene = new Scene(root);  // apply stuff wanna show to scene
 
-        stage.setScene(scene);  // set up scene
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
 
-        stage.show(); // time for performing
+        window.setScene(scene);  // set up scene
+
+        window.show(); // time for performing
 
     }
 }
