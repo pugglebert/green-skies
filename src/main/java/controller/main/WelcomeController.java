0ec054234e8b64db16236ed983c8df22aa2c804d
Hayley Krippner <hek21@uclive.ac.nz>
2020-08-17 11:27:20 +1200

    Hayley: fixed overridden upload scene code.

--- src/main/java/controller/main/WelcomeController.java
+++ src/main/java/controller/main/WelcomeController.java
@@ -12,23 +12,24 @@ import java.io.IOException;
 public class WelcomeController {
 
     //Method to handle the event for continuing to the main screen of the application.
-    public void continueToMainScreen() {
+    public void continueToMainScreen() throws IOException {
+        Stage stage = new Stage(); // create a stage for scene
+        Parent root = FXMLLoader.load(getClass().getResource("upload.fxml")); // load upload.fxml
+        Scene scene = new Scene(root, 1024, 640);  // apply stuff wanna show to scene
+        stage.setResizable(false);
+        stage.setScene(scene);  // set up scene
+        stage.show(); // time for performing
+        }
 
-    }
 
     //Method to handle the event for continuing to the info screen of the application.
     public void toInfoScreen(ActionEvent event) throws IOException {
-
-
+        Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("help.fxml")); // load help.fxml
-
-        Scene scene = new Scene(root);  // apply stuff wanna show to scene
-
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(scene);  // set up scene
-
-        window.show(); // time for performing
+        Scene scene = new Scene(root, 1024, 640);  // apply stuff wanna show to scene
+        stage.setResizable(false);
+        stage.setScene(scene);  // set up scene
+        stage.show(); // time for performing
 
     }
 }
