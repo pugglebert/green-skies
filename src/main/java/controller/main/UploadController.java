41f3804e7d58e51c2855f5b28d12a1dfb672942a
Grace Hanlon <gmh91@uclive.ac.nz>
2020-08-19 09:45:09 +1200

    Grace Hanlon - back button added to upload page.

--- src/main/java/controller/main/UploadController.java
+++ src/main/java/controller/main/UploadController.java
@@ -44,17 +44,30 @@ public class UploadController extends Application {
         dataTypeSelect.getItems().addAll(dataTypeList);
     }
 
+    //opens file browser when button pushed
     public void browseFiles() {
-        FileChooser fileChooser = new FileChooser();
+        FileChooser fileChooser = new FileChooser(); //opens a file local file browser
         File selectedFile = fileChooser.showOpenDialog(null);
-        if (selectedFile != null){
+        if (selectedFile != null){  // check a file has been selected
             fileView.getItems().add(selectedFile.getName());
-            nextButton.setVisible(true);
+            nextButton.setVisible(true); //'Next' button pops up if a valid file has been selected
         } else{
             fileErrorText.setVisible(true);
 
         }
     }
+
+    //take user back to the welcome screen in case of wanting to see info screen
+    public void backToWelcome() throws IOException {
+        Stage stage = new Stage(); // create a stage for scene
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml")); // load welcome.fxml
+        Scene scene = new Scene(root, 1024, 640);  // apply stuff wanna show to scene
+        stage.setResizable(false);
+        stage.setScene(scene);  // set up scene
+        stage.show(); // time for performing
+    }
+
+
 
     public static void main(String[] args) {
         launch(args);
