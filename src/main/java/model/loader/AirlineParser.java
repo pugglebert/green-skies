f95f71a3b18f1183df2e578c777b4fe5a40a1462
nhanh <tnh33>
2020-08-23 03:10:00 +1200

    Update regex of Airline

--- src/main/java/model/loader/AirlineParser.java
+++ src/main/java/model/loader/AirlineParser.java
@@ -3,8 +3,8 @@ package model.loader;
 import model.data.Airline;
 import model.data.DataType;
 
-import java.util.List;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class AirlineParser extends Parser {
@@ -12,45 +12,31 @@ public class AirlineParser extends Parser {
   private final Set<DataType> airlines = new HashSet<>();
   // Alphabetical name to represent line index
   private final int airlineID = 0,
-          name = 1,
-          alias = 2,
-          IATA = 3,
-          ICAO = 4,
-          callsign = 5,
-          country = 6,
-          activeStatus = 7;
+      name = 1,
+      alias = 2,
+      IATA = 3,
+      ICAO = 4,
+      callsign = 5,
+      country = 6,
+      activeStatus = 7;
 
   public AirlineParser(List<String> dataFile) {
     super(dataFile, 12);
+
     /**
-     * AirportParser Error code:
-     * 100: not enough parameters
-     * 101: airport id exists
-     * 102: invalid id number
-     * 103: invalid airport name
-     * 104: invalid airport city
-     * 105: invalid airport country
-     * 106: invalid airport IATA code
-     * 107: invalid airport ICAO code
-     * 108: invalid latitude
-     * 109: invalid lontitude
-     * 110: invalid altitude
-     * 111: invalid timezone
-     * 112: invalid DST
-     * 113: invalid database timezone
-     * 114: invalid unknown error 115: number of failed insertions
+     * AirportParser Error code: 100: not enough parameters 101: airport id exists 102: invalid id
+     * number 103: invalid airport name 104: invalid airport city 105: invalid airport country 106:
+     * invalid airport IATA code 107: invalid airport ICAO code 108: invalid latitude 109: invalid
+     * lontitude 110: invalid altitude 111: invalid timezone 112: invalid DST 113: invalid database
+     * timezone 114: invalid unknown error 115: number of failed insertions
      *
-     * <p>116: invalid alias
-     * 117: invalid callsign
-     * 118: invalid activestatus
+     * <p>116: invalid alias 117: invalid callsign 118: invalid activestatus
      */
     dataParser();
   }
 
   @Override
-  /**
-   * Initializes error lookup array with message for each error code.
-   */
+  /** Initializes error lookup array with message for each error code. */
   protected void initErrorLookup() {
     errorLookup[0] = "Not enough parameters";
     errorLookup[1] = "Duplicate airline";
@@ -67,22 +53,28 @@ public class AirlineParser extends Parser {
   }
 
   public void dataParser() {
+
     for (String dataLine : dataFile) {
+      dataLine = dataLine.replaceAll("[\"]", "");
       String[] line = dataLine.split(",");
-      // need if(validate(line))
+
+      //System.out.println("array: " + Arrays.toString((line)));
+
       if (validater(line)) {
+
         try {
           Airline airline =
-                  new Airline(
-                          Integer.parseInt(line[airlineID]),
-                          line[name],
-                          line[alias],
-                          line[IATA],
-                          line[ICAO],
-                          line[callsign],
-                          line[country],
-                          Boolean.parseBoolean(line[activeStatus]));
+              new Airline(
+                  Integer.parseInt(line[airlineID]),
+                  line[name],
+                  line[alias],
+                  line[IATA],
+                  line[ICAO],
+                  line[callsign],
+                  line[country],
+                  Boolean.parseBoolean(line[activeStatus]));
           airlines.add(airline);
+
         } catch (Exception e) {
           errorCounter(10);
         }
@@ -99,34 +91,42 @@ public class AirlineParser extends Parser {
     }
 
     if (!isIdValid(line[airlineID])) {
+      System.out.println("ID " + line[airlineID]);
       isValid = false;
     }
 
     if (!isNameValid(line[name])) {
+      System.out.println("name " + line[name]);
       isValid = false;
     }
 
     if (!isAliasValid(line[alias])) {
+      System.out.println("alias " + line[alias]);
       isValid = false;
     }
 
     if (!isIATAValid(line[IATA])) {
+      System.out.println("IATA " + line[IATA]);
       isValid = false;
     }
 
     if (!isICAOValid(line[ICAO])) {
+      System.out.println("ICAO " + line[ICAO]);
       isValid = false;
     }
 
     if (!isCallsignValid(line[callsign])) {
+      System.out.println("callsign " + line[callsign]);
       isValid = false;
     }
 
     if (!isCountryValid(line[country])) {
+      System.out.println("country " + line[country]);
       isValid = false;
     }
 
     if (!isActiveStatusValid(line[activeStatus])) {
+      System.out.println("activeStatus " + line[activeStatus]);
       isValid = false;
     }
     return isValid;
@@ -138,11 +138,13 @@ public class AirlineParser extends Parser {
    * @return true if valid, false if invalid.
    */
   private boolean isIdValid(String airlineID) {
+
     // airline ID Duplication check
     for (DataType data : airlines) {
+
       try {
         Airline airline = (Airline) data;
-        if (airline.getAirlineID() == Integer.parseInt(airlineID)) {
+        if (airline.getAirlineID() == Integer.parseInt(airlineID) && airline.getAirlineID() >= 0) {
           errorCounter(1);
           return false;
         }
@@ -161,7 +163,7 @@ public class AirlineParser extends Parser {
    * @return true if valid, false if invalid.
    */
   private boolean isNameValid(String name) {
-    if (!name.matches("[a-zA-Z ]+")) {
+    if (!name.matches("[a-zA-Z0-9 -.]+")) {
       errorCounter(3);
       return false;
     }
@@ -175,7 +177,7 @@ public class AirlineParser extends Parser {
    * @return true if valid, false if invalid.
    */
   private boolean isAliasValid(String alias) {
-    if (!alias.matches("(([a-zA-Z][ ]?)+)|(\\\\N)")) {
+    if (!alias.matches("(\\\\N)|([\\w ]+)|(^$)")) {
       errorCounter(4);
       return false;
     }
@@ -189,13 +191,13 @@ public class AirlineParser extends Parser {
    * @return true if valid, false if invalid.
    */
   private boolean isIATAValid(String IATA) {
+
     // airline IATA check
-    if (!IATA.equalsIgnoreCase("null") && !IATA.equalsIgnoreCase("unknown")) {
-      if (!IATA.matches("[a-zA-Z]+") || IATA.length() != 2) {
-        errorCounter(5);
-        return false;
-      }
+    if (!IATA.matches("-|([A-Z0-9]{2})|(^$)")) {
+      errorCounter(5);
+      return false;
     }
+
     return true;
   }
 
@@ -206,13 +208,14 @@ public class AirlineParser extends Parser {
    * @return true if valid, false if invalid.
    */
   private boolean isICAOValid(String ICAO) {
+
     // airline ICAO check
-    if (!ICAO.equalsIgnoreCase("null") && !ICAO.equalsIgnoreCase("unknown")) {
-      if (!ICAO.matches("[a-zA-Z]+") || ICAO.length() != 3) {
+
+      if (!ICAO.matches("(\\\\N)|(N/A)|([A-Z0-9]{3})|(^$)")) {
         errorCounter(6);
         return false;
       }
-    }
+
     return true;
   }
 
@@ -224,7 +227,7 @@ public class AirlineParser extends Parser {
    */
   private boolean isCallsignValid(String callsign) {
     // airline callsign check
-    if (!callsign.matches("(([A-Z][ ]?)+)|$")) {
+    if (!callsign.matches("(\\\\N)|([A-Za-z -]+)|(^$)")) {
       errorCounter(7);
       return false;
     }
@@ -239,7 +242,7 @@ public class AirlineParser extends Parser {
    */
   private boolean isCountryValid(String country) {
     // ISO 3166-1 codes not implemented
-    if (!country.matches("[a-zA-Z ]+")) {
+    if (!country.matches("(\\\\N)|([a-zA-Z ]+)|(^$)")) {
       errorCounter(8);
       return false;
     }
@@ -253,6 +256,7 @@ public class AirlineParser extends Parser {
    * @return true if valid, false if invalid.
    */
   private boolean isActiveStatusValid(String activeStatus) {
+
     if (!activeStatus.matches("[YN]")) {
       errorCounter(9);
       return false;
@@ -268,4 +272,20 @@ public class AirlineParser extends Parser {
   public Set<DataType> getData() {
     return airlines;
   }
+
+//  public static void main(String[] args) throws Exception {
+//    ArrayList<String> testLines;
+//    testLines = new ArrayList<String>();
+//
+//    BufferedReader br = new BufferedReader(new FileReader("src/test/java/TestFiles/airlines.csv"));
+//    int count = 0;
+//    String line = "";
+//    br.readLine(); // header
+//    while ((line = br.readLine()) != null && count < 1000) {
+//      testLines.add(line);
+//      //count++;
+//    }
+//
+//    AirlineParser parser = new AirlineParser(testLines);
+//  }
 }
