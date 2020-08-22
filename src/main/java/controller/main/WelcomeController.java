c69acfa074321df647eb1f9fcc37619cf5cbda01
He zhengjingrui <zhe25@uclive.ac.nz>
2020-08-22 14:52:48 +1200

    Tom made some change to welcomeController

--- src/main/java/controller/main/WelcomeController.java
+++ src/main/java/controller/main/WelcomeController.java
@@ -1,6 +1,8 @@
 package controller.main;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -21,13 +23,12 @@ public class WelcomeController {
     }
 
     //Method to handle the event for continuing to the info screen of the application.
-    public void toInfoScreen() throws IOException {
-        Stage stage = new Stage(); // create a stage for scene
+    public void toInfoScreen(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("help.fxml")); // load help.fxml
         Scene scene = new Scene(root, 1024, 640);  // apply stuff wanna show to scene
-        stage.setResizable(false);
-        stage.setScene(scene);  // set up scene
-        stage.show(); // time for performing
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);  // set up scene
+        window.show(); // time for performing
 
     }
 
