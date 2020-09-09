db12e3c464d3dd14db8920b12b08b3591c8015e9
Hayley Krippner <hek21@uclive.ac.nz>
2020-09-09 22:06:47 +1200

    Hayley: changed the Upload page from grid pane to anchor pane and manually tested that all the buttons worked. I also made all the pages the same size so they are consistent. I discovered that the user is prompted with the error dialog box upon attempting to upload a file which needs to be resolved and tested. The error message in the top right corner needs to be removed and displayed in the error box too.

--- src/main/java/controller/main/WelcomeController.java
+++ src/main/java/controller/main/WelcomeController.java
@@ -17,18 +17,19 @@ public class WelcomeController {
     public void continueToMainScreen(ActionEvent event) throws IOException {
 
         Parent root = FXMLLoader.load(getClass().getResource("upload.fxml")); // load upload.fxml
-        Scene scene = new Scene(root, 1024, 640);  // apply stuff wanna show to scene
+        Scene scene = new Scene(root, 1400, 700);  // apply stuff wanna show to scene
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(scene);  // set up scene
         window.show(); // time for performing
 
     }
 
+
     //Method to handle the event for continuing to the info screen of the application.
     public void toInfoScreen(ActionEvent event) throws IOException {
         // TODO change to "help.fxml" this is debug
         Parent root = FXMLLoader.load(getClass().getResource("help.fxml")); // load help.fxml
-        Scene scene = new Scene(root, 1024, 640);  // apply stuff wanna show to scene
+        Scene scene = new Scene(root, 1500, 800);  // apply stuff wanna show to scene
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(scene);  // set up scene
         window.show(); // time for performing
