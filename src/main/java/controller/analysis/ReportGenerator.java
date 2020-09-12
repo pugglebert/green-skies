df484c9f65872aeb3bb6941991381098a3bcad25
Hayley Krippner <hek21@uclive.ac.nz>
2020-09-13 01:03:41 +1200

    Hayley: implemented the updateMostTravelledRoute, updateLeastTravelledRoute, MostVisitedSrcAirports, MostVisitedDestAirports, binarySearch (with just looking for an int e.g. ID), quickSortPartition,quickSort. There is a lot of repetition in the first four methods mentioned so I would like to create a generate function for these next. I am aware that doing a linear search is not scalable so have decided to use quicksort and binary search to overcome this. I implemented a basic version of these and will apply it to the different needs of it next and test them as I go.

--- src/main/java/controller/analysis/ReportGenerator.java
+++ src/main/java/controller/analysis/ReportGenerator.java
@@ -1,14 +1,14 @@
 package controller.analysis;
 
-import model.data.Airport;
 import model.data.Route;
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Class for which contains the methods for calcuting and updating the analysis data for the user's carbon emissions report.
  * @author Hayley Krippner
- * @since 12/09/20
+ * @since 13/09/20
  * @version 1.0
  */
 public class ReportGenerator {
@@ -25,9 +25,13 @@ public class ReportGenerator {
 
     private ArrayList<Route> leastDistanceRoutes;
 
-    private ArrayList<Airport> MostVisitedSrcAirports;
+    private ArrayList<String> mostVisitedSrcAirports;
 
-    private ArrayList<Airport> MostVisitedDestAirports;
+    private ArrayList<String> mostVisitedDestAirports;
+
+    private ArrayList<Route>  mostTravelledRoutes;
+
+    private ArrayList<Route>  leastTravelledRoutes;
 
     private double carbonEmissionGoal; // grams
 
@@ -58,22 +62,35 @@ public class ReportGenerator {
     //TODO: write this method.
     /**
      * This method updates the current most travelled flight route.
-     * @param currentRouteRecord, the current route record that is being added to user's flight history.
-     * @param timesRouteTaken, the times the route has been taken.
+     * @param routeHistoryEntries, the current route record that is being added to user's flight history.
      */
-    public void updateMostTravelledRoute(Route currentRouteRecord, int timesRouteTaken) {
-
+    public void updateMostTravelledRoute(ArrayList<Route> routeHistoryEntries) {
+        int currMaxRouteUsed = 0;
+        for (Route route : routeHistoryEntries) {
+            if (route.getTimesTake() > currMaxRouteUsed) {
+                mostTravelledRoutes.clear();
+                mostTravelledRoutes.add(route);
+            } else if (route.getTimesTake() == currMaxRouteUsed) {
+                mostTravelledRoutes.add(route);
+            }
+        }
     }
 
     //TODO: write this method.
     /**
      * This method updates the current least travelled flight route, provided the route is already in the flight history.
-     * @param currentRouteRecord, the current route record that is being added to user's flight history.
+     * @param routeHistoryEntries, the current route record that is being added to user's flight history.
      */
-    public void updateLeastTravelledRoute(Route currentRouteRecord) {
-//        for (Route entry : flightRouteHistory) {
-//
-//        }
+    public void updateLeastTravelledRoute(ArrayList<Route> routeHistoryEntries) {
+        int currMaxRouteUsed = 0;
+        for (Route route : routeHistoryEntries) {
+            if (route.getTimesTake() > currMaxRouteUsed) {
+                leastTravelledRoutes.clear();
+                leastTravelledRoutes.add(route);
+            } else if (route.getTimesTake() == currMaxRouteUsed) {
+                leastTravelledRoutes.add(route);
+            }
+        }
     }
 
     /**
@@ -139,16 +156,45 @@ public class ReportGenerator {
         }
     }
 
-    //TODO: write this method.
-    public void MostVisitedSrcAirports(ArrayList<Route> flightRouteHistory) {
+  // TODO: write a single method for MostVisitedSrcAirports and MostVisitedDestAirports
 
+  /**
+   * Calculates the airport(s) that was the most visited, based on the user's flight history entries.
+   * @return mostVisitedSrcAirports, an ArrayList containing the String of the name of the airports
+   *     that were most visited, based on the user's flight history entries.
+   */
+  public void MostVisitedSrcAirports(HashMap<String, Integer> srcAirportCounts) {
+        int currSrcAirportMax = 0;
+        for (Map.Entry<String, Integer> entry : srcAirportCounts.entrySet()) {
+            if (entry.getValue() > currSrcAirportMax) {
+                currSrcAirportMax = entry.getValue();
+                mostVisitedSrcAirports.clear();
+                mostVisitedSrcAirports.add(entry.getKey());
+            } else if (entry.getValue() == currSrcAirportMax) {
+                currSrcAirportMax = entry.getValue();
+                mostVisitedSrcAirports.add(entry.getKey());
+            }
         }
-
-
-    //TODO: write this method.
-    public void MostVisitedDestAirports(ArrayList<Route> flightRouteHistory) {
-
     }
+
+/**
+ * Calculates the airport(s) that was the least visited, based on the user's flight history entries.
+ * @return mostVisitedSrcAirports, an ArrayList containing the String of the name of the airports
+ *     that were most visited, based on the user's flight history entries.
+ */
+  public void MostVisitedDestAirports(HashMap<String, Integer> destAirportCounts) {
+    int currDestAirportMax = 0;
+    for (Map.Entry<String, Integer> entry : destAirportCounts.entrySet()) {
+      if (entry.getValue() > currDestAirportMax) {
+        currDestAirportMax = entry.getValue();
+        mostVisitedDestAirports.clear();
+        mostVisitedDestAirports.add(entry.getKey());
+      } else if (entry.getValue() == currDestAirportMax) {
+        currDestAirportMax = entry.getValue();
+        mostVisitedDestAirports.add(entry.getKey());
+      }
+    }
+  }
 
     /**
      * This method calcuates the amount of CO2 that the user can produce whilst meeting their maximum yearly CO2
@@ -191,12 +237,12 @@ public class ReportGenerator {
         return leastEmissionsRoutes;
     }
 
-    public ArrayList<Airport> getMostVisitedSrcAirports() {
-        return MostVisitedSrcAirports;
+    public ArrayList<String> getMostVisitedSrcAirports() {
+        return mostVisitedSrcAirports;
     }
 
-    public ArrayList<Airport> getMostVisitedDestAirports() {
-        return MostVisitedDestAirports;
+    public ArrayList<String> getMostVisitedDestAirports() {
+        return mostVisitedDestAirports;
     }
 
     public double getCarbonEmissionGoal() {
@@ -209,6 +255,65 @@ public class ReportGenerator {
 
     public double getAnalysisPeriod() {
         return analysisPeriod;
+    }
+
+    /**
+     * This function implements the binary search algorithm.
+     * @param arraryToSearch, the array which is being searched.
+     * @param searchElement, the element that is being search for.
+     * @return
+     */
+    public static int binarySearch(int arraryToSearch[], int searchElement) {
+        int firstIndex = 0;
+        int lastIndex = arraryToSearch.length - 1;
+        while(firstIndex <= lastIndex) {
+            int middleIndex = (firstIndex + lastIndex) / 2;
+            if (arraryToSearch[middleIndex] == searchElement) {
+                return middleIndex;
+            }
+            else if (arraryToSearch[middleIndex] < searchElement)
+                firstIndex = middleIndex + 1;
+            else if (arraryToSearch[middleIndex] > searchElement)
+                lastIndex = middleIndex - 1;
+        }
+        return -1;
+    }
+
+    /**
+     * This method sets up the quick sort algorithm.
+     * @param arraytoSort , the array which needs to be sorted.
+     * @param start , the starting index of the arrayToSort.
+     * @param end , the ending index of the arrayToSort.
+     */
+    public static void quickSort(int[] arraytoSort, int start, int end) {
+        if (end <= start) return;
+        int pivot = quickSortPartition(arraytoSort, start, end);
+        quickSort(arraytoSort, start, pivot-1);
+        quickSort(arraytoSort, pivot+1, end);
+    }
+
+    /**
+     * This function implements the main logic of the quick sort algoirthm.
+     * @param arraytoSort , the array which needs to be sorted.
+     * @param start , the starting index of the arrayToSort.
+     * @param end , the ending index of the arrayToSort.
+     * @return
+     */
+    static int quickSortPartition(int[] arraytoSort, int start, int end) {
+        int pivot = end;
+        int counter = start;
+        for (int i = start; i < end; i++) {
+            if (arraytoSort[i] < arraytoSort[pivot]) {
+                int temp = arraytoSort[counter];
+                arraytoSort[counter] = arraytoSort[i];
+                arraytoSort[i] = temp;
+                counter++;
+            }
+        }
+        int temp = arraytoSort[pivot];
+        arraytoSort[pivot] = arraytoSort[counter];
+        arraytoSort[counter] = temp;
+        return counter;
     }
 
 }
