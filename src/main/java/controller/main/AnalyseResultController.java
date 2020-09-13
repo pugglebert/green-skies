2628a8e0912e9fb7f30f15b328b29aa6f5238fa5
He zhengjingrui <zhe25@uclive.ac.nz>
2020-09-14 01:36:02 +1200

    Tom add AnalyseResultController, and try to show the distance and Emission on textfield

--- src/main/java/controller/main/AnalyseResultController.java
+++ src/main/java/controller/main/AnalyseResultController.java
@@ -2,13 +2,34 @@ package controller.main;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import controller.main.AnalyseController;
 
 public class AnalyseResultController {
 
     @FXML
     private Button BackButton;
 
-    public void  GoBackToAnalyse(){
+    @FXML
+    public TextField DistanceRoute1;
 
+    @FXML
+    public TextField DistanceRoute2;
+
+    @FXML
+    public TextField EmissionRoute1;
+
+    @FXML
+    public TextField EmissionRoute2;
+
+
+    @FXML
+    private void  GoBackToAnalyse(){
+        Stage stage = (Stage) BackButton.getScene().getWindow();   //get current window
+        stage.close();  // close current window
     }
+
+
 }
