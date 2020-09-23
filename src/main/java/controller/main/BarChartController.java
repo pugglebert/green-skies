31bd24cda45013e9e6facef2b43f523811c47897
Grace Hanlon <gmh91@uclive.ac.nz>
2020-09-23 21:56:36 +1200

    Grace Hanlon - got very basic bar chart showing up with preloaded sample data

--- src/main/java/controller/main/BarChartController.java
+++ src/main/java/controller/main/BarChartController.java
@@ -1,38 +1,43 @@
 package controller.main;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.BorderPane;
 
-public class BarChartController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BarChartController implements Initializable {
 
     @FXML
     BorderPane borderPane;
+    @FXML
+    NumberAxis yAxis;
+    @FXML
+    CategoryAxis xAxis;
+    @FXML
+    BarChart<String, Number> barChart;
 
-    public void showChart() {
-        CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Route");
 
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Carbon Emissions");
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+        //xAxis.setLabel("Route");
+        //yAxis.setLabel("Carbon Emissions");
 
-        BarChart barChart = new BarChart(xAxis, yAxis);
+        XYChart.Series<String, Number> data = new XYChart.Series();
 
-        XYChart.Series data = new XYChart.Series();
-        data.setName("Routes Taken");
-
-        //provide data
+        //provide data EXAMPLES
         data.getData().add(new XYChart.Data("Route A", 100));
         data.getData().add(new XYChart.Data("Route b", 111));
         data.getData().add(new XYChart.Data("Route c", 50));
 
         barChart.getData().add(data);
-
-        borderPane.setCenter(barChart);
     }
+
 
 
 }
