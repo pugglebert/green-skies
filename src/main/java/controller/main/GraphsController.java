5dad1bcfbcff5cc57d15ae149bfee35e7bac36d1
Grace Hanlon <gmh91@uclive.ac.nz>
2020-09-25 12:22:27 +1200

    Grace Hanlon - graph displays only top ten routes from history - bug needs to be fixed where the graph wont display if there is an emissions value of NaN

--- src/main/java/controller/main/GraphsController.java
+++ src/main/java/controller/main/GraphsController.java
@@ -5,6 +5,7 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.text.Text;
 import model.data.Route;
 
 import java.net.URL;
@@ -26,8 +27,11 @@ public class GraphsController extends SideNavBarController {
   CategoryAxis xAxis;
   @FXML
   BarChart<String, Number> barChart;
+  @FXML
+  Text warningText;
 
   List<Route> routes;
+  private double NaN;
 
   /**
    * This method is the initializer for this class.
@@ -42,27 +46,51 @@ public class GraphsController extends SideNavBarController {
 
     routes = storage.getHistory();
 
-//    if (routes.size() <= 10) {
-//      for (Route route : routes) {
-//        String dest = route.getDestinationAirport();
-//        String src = route.getSourceAirport();
-//        String axisString = String.format("%s - %s", src, dest);
-//        double emissions = route.getEmissions();
-//        data.getData().add(new XYChart.Data(axisString, emissions));
-//      }
-//    } else {
-      List<Route> sortedByEmissions = new ArrayList<Route>(routes);
-      Collections.sort(sortedByEmissions, new Comparator<Route>() {
-        @Override
-        public int compare(Route o1, Route o2) {
-          return Double.valueOf(o1.getEmissions()).compareTo(o2.getEmissions());
-        }
-      });
-      System.out.println(sortedByEmissions);
-    //}
+    if (routes.size() == 0) {
+      warningText.setVisible(true);
+    } else {
 
-    //barChart.getData().add(data);
-  }
+      if (routes.size() <= 10) {
+        for (Route route : routes) {
+          String dest = route.getDestinationAirport();
+          String src = route.getSourceAirport();
+          String axisString = String.format("%s - %s", src, dest);
+          double emissions = route.getEmissions();
+          data.getData().add(new XYChart.Data(axisString, emissions));
+        }
+      } else {
+        ArrayList<Route> sortedByEmissions = new ArrayList<Route>(routes);
+        Collections.sort(
+            sortedByEmissions,
+            new Comparator<Route>() {
+              @Override
+              public int compare(Route o1, Route o2) {
+                return Double.valueOf(o2.getEmissions()).compareTo(o1.getEmissions());
+              }
+            });
+        int i = 0;
+        int limit = 10;
+        while(i<limit) {
+          if (sortedByEmissions.get(i).getEmissions() != NaN) { //issue, graph wont display if there is NaN for emissions value
+            String dest = sortedByEmissions.get(i).getDestinationAirport();
+            String src = sortedByEmissions.get(i).getSourceAirport();
+            String axisString = String.format("%s - %s", src, dest);
+            double emissions = sortedByEmissions.get(i).getEmissions();
+            data.getData().add(new XYChart.Data(axisString, emissions));
+            i += 1;
+            System.out.println(emissions);
+          } else {
+            limit += 1;
+            i += 1;
+          }
+        }
+
+
+      }
+
+      barChart.getData().add(data);
+    }
+    }
 
 
 
