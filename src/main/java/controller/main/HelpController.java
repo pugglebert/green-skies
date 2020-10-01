cb5d2a2a1f92dbb220a5c3fb0f4689faaaebe9f9
Grace Hanlon <gmh91@uclive.ac.nz>
2020-10-01 23:37:12 +1300

    Grace Hanlon - fixed back button on info page

--- src/main/java/controller/main/HelpController.java
+++ src/main/java/controller/main/HelpController.java
@@ -1,5 +1,6 @@
 package controller.main;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -25,10 +26,12 @@ public class HelpController {
    *
    * @throws IOException This throws an IOException.
    */
+  @FXML
   public void BackToPreviousWindow() throws IOException {
     Stage stage1 = GreenSkiesApplication.getPrimaryStage();
-    Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("/view/welcome.fxml"));
     Scene scene = new Scene(root);
     stage1.setScene(scene);
   }
 }
+
